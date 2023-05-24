5736dfdf7c1b2a6b707acd09e2ad033dbc2e7974
christian <cgvasconez2@espe.edu.ec>
2023-05-24 07:44:58 -0500

    Curso de Python

--- Python/primitivos/compuestos.py
+++ Python/primitivos/compuestos.py
@@ -37,3 +37,44 @@ print(lista2)
 lista2 = ["a","b","B","c","d"]
 lista2.sort()
 print(lista2)
+
+#TUPLA
+
+tupla = ("Quito","Guayaquil","Cuenca","Quito")
+print(type(tupla)) #No se pueden modificar
+
+# tupla.append("Machala")
+# print(tupla) 
+
+print (tupla.count("Quito"))
+print (tupla.index("Cuenca"))
+# print (tupla.index("Quito"))
+
+lista = list(tupla)
+print(lista)
+print(tupla)
+
+lista.append("Machala")
+tupla = tuple(lista)
+
+print(lista)
+print(tupla)
+
+#RANGO
+
+rango = range(10)
+print (type(rango))
+print (rango)
+
+#SETS
+
+conjunto = {"i","e", "u", "o", "i", "a"}
+print(type(conjunto))
+print(conjunto)
+
+#DICCIONARIOS
+
+cliente001 = {}
+
+
+
